7dfec95bc2e669a507b9bf90ca0785e0fbe4e070
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-12 09:31:28 -0400

    Style improvements

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,21 +1,10 @@
-// Variables for holding data
-
-// HTML elements needed
-// 1. form for entering data ** DONE
-// 2. table for displaying data dynamically -- both on intialization of page, when app renders data from database, and when user enters new train information into the database ** DONE
-// 3. place for modal to display ** DONE
-
-// JS elements and functions needed
-// 1. configuration of firebase database ** DONE
-// 2. initiailization of firebase database ** DONE
-// 3. funciton to push form data to database (new trains) -- to write new information to the DOM -- use display news articles example from newsreader ** DONE
-// 4. function to pull data from database (when app launched) -- use create buttons example from newsreader  ** DONE
-// 5. function to calculate next arrival (moment.js)
-// 6. function to calculate minutes away (moment.js)
-// 7. modal for alerting user to successful entry ** DONE
-
 // Other notes
-// 1. don't forget to load moment.js library
+// 1. don't forget to load moment.js library ** DONE
+// 2. clean up modal
+// 3. general stylistic upgrades
+// 4. Too many global variables: move any many as possible out of global
+// 5. Center data in last three columns
+// 6. Table heading should be in color
 
 var frequency = 0;
 var firstRun = "";
@@ -31,8 +20,6 @@ var destination = "";
 var frequency = "";
 var firstRun = "";
 
-
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyCkxMF68Nyb17ZEwQKpeU9e7Y0I-vfz8Jk",
     authDomain: "sodortrains-79f02.firebaseapp.com",
@@ -63,17 +50,10 @@ database.ref().on("child_added", function (snapshot) {
     var sv = snapshot.val();
     var newTrainDiv = $("<tr>");
     firstTrain = moment(sv.firstRunFB, "HH:mm").subtract(1, "years");
-    console.log("this is the first train time: " + firstTrain);
-    console.log("this is the time of the first run, from firebase: " + sv.firstRunFB);
     elapsedMinutes = moment().diff(moment(firstTrain), "minutes")
-    console.log("these are the minutes elapsed since the train began running: " + elapsedMinutes);
-    console.log("this is how often the train runs, in minutes: " + sv.frequencyFB);
     prochainTrain = elapsedMinutes % sv.frequencyFB;
-    console.log("this is the remainder - % - of elapsed minutes-frequency: " + prochainTrain);
     toStation = sv.frequencyFB - prochainTrain;
-    console.log("this is the time it will take the train to get to the station: " + toStation);
     nextArrival = moment().add(toStation, 'minutes').format('hh:mm');
-    console.log("this is the time at which the next train will arrive: " + nextArrival);
     var name = $("<td>").text(sv.trainNameFB);
     var dest = $("<td>").text(sv.destinationFB);
     var freq = $("<td>").text(sv.frequencyFB);
@@ -97,5 +77,3 @@ function modalLauncher() {
         modal.style.display = "none";
     }
 }
-
-
